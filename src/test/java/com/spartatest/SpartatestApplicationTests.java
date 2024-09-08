4617fe59116d7528b7c3e0314288b191ac4e8fdc
최윤석 <97040581+Yundae203@users.noreply.github.com>
2024-09-08 17:38:29 +0900

    remove test annotation

--- src/test/java/com/spartatest/SpartatestApplicationTests.java
+++ src/test/java/com/spartatest/SpartatestApplicationTests.java
@@ -3,10 +3,10 @@ package com.spartatest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SpartatestApplicationTests {
 
-    @Test
+    // @Test
     void contextLoads() {
     }
 
